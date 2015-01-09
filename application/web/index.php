cdfc05bf1cccdf9f9bcc5ceae1f898adafac3695
Rob Bast <rob.bast@gmail.com>
2015-01-09 14:06:39 +0100

    use apcclassloader on production if available

--- application/web/index.php
+++ application/web/index.php
@@ -4,6 +4,7 @@ $loader = require_once __DIR__ . '/../vendor/autoload.php';
 
 use Alcohol\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\ClassLoader\ApcClassLoader;
 
 Dotenv::makeMutable();
 Dotenv::load(__DIR__ . '/../');
@@ -17,7 +18,14 @@ Dotenv::required([
     'SYMFONY__REDIS__PORT',
 ]);
 
+if (extension_loaded('apc') && in_array(getenv('SYMFONY_ENV'), ['prod'])) {
+    $apcloader = new ApcClassLoader(sha1(__FILE__), $loader);
+    $loader->unregister();
+    $apcloader->register(true);
+}
+
 $application = new Application(getenv('SYMFONY_ENV'), getenv('SYMFONY_DEBUG'));
+$application->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $application->handle($request);
 $response->send();
