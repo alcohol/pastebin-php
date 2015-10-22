8c6d7e35880d004b8d6e5b5f5de9817084db0fb4
Rob Bast <rob.bast@gmail.com>
2015-10-22 12:51:06 +0200

    modified php-cs-fixer rules

--- source/Application.php
+++ source/Application.php
@@ -1,12 +1,5 @@
 <?php
 
-/*
- * (c) Rob Bast <rob.bast@gmail.com>
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
-
 namespace Alcohol\Paste;
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
