8de8f59024d54bd252dd320e9c3409b57c6951aa
Rob Bast <rob.bast@gmail.com>
2018-02-26 14:03:11 +0100

    add health endpoint

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+/*
+ * (c) Rob Bast <rob.bast@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Paste\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+final class HealthController
+{
+    public function __invoke(): Response
+    {
+        return new Response();
+    }
+}
---
+++ tests/integration/Controller/HealthControllerTest.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+/*
+ * (c) Rob Bast <rob.bast@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Paste\Controller;
+
+use Paste\IntegrationTest;
+
+/**
+ * @group integration
+ */
+class HealthControllerTest extends IntegrationTest
+{
+    public function test_it_should_return_a_200_response_for_HEAD_requests()
+    {
+        $client = static::createClient();
+        $client->request('HEAD', '/health');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+
+    public function test_it_should_return_a_200_response_for_GET_requests()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/health');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+}
