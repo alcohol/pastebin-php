eb2dabad4706e7c2515281331ee62ca96a228924
Rob Bast <rob.bast@gmail.com>
2021-10-25 09:55:55 +0200

    add missing headers

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Rob Bast <rob.bast@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
